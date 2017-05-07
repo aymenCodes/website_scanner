5480a194e2f3299cb8ffd7e9e7259e93d8d8c85e
AymenDz <benchiheub.aymen@gmail.com>
2017-05-07 15:22:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,21 +4,21 @@ from tld import get_tld
 
 def get_local_time():
     date = time.localtime()
-    day = "%d_%d_%d"%(date[1],date[2],date[0])
-    return (day)
+    day = "__%d_%d_%d"%(date[1],date[2],date[0])
+    return day
 
 def get_domain(url):
     # Just to Get The Domain Name !
     #example : google.com
     url = get_tld(url)
-    return(url)
+    return url
 
 def get_ip(domain):
     # To Get The Ip adress
     command = "host " + domain
     process = os.popen(command)
     reusalt = str(process.read())
-    return(reusalt)
+    return reusalt
 
 def nmap(options,ip):
     # We Will scan The Websit By Nmap Tool
@@ -27,7 +27,7 @@ def nmap(options,ip):
     command = 'nmap ' + 'options ' + str(ip)
     process = os.popen(command)
     reusalt = str(process.read())
-    return(reusalt)
+    return reusalt
 
 def whois(ip):
     # We Will scan The Websit By Whois Tool
@@ -36,25 +36,25 @@ def whois(ip):
     command = "whois " + ip
     process = os.popen(command)
     reusalt = str(process.read())
-    return(reusalt)
+    return reusalt
 
 def create_dir(directory):
-    # Here Will Creat A folder if is Not exists
+    # Here we Will Creat A folder if is Not exists
     if not os.path.exists(directory):
         os.mkdir(directory)
         os.chdir(directory)
 
 def write_file(name,data):
     # Create The TXT file where we will put
-    # The Whole Informaions About The Websit
+    # The Whole INFORMATIONS About The Website
     file = open(name + '.txt','a+')
     file.write(str(data) + "\n")
     file.close()
-    
+
 def bye():
     print('Exit Now...')
     time.sleep(1)
-    print('Bye Press Enter To Exit !')
+    print('Bye Press Enter To Exit...')
     x = input()
     exit()
 # __The End__
